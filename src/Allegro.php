1f242314599dc377cc2ec5cb440882efdfb0f50b
Patryk <32879043+ReqqQ@users.noreply.github.com>
2019-11-27 14:27:16 +0100

    update allegro and create order folder

--- src/Allegro.php
+++ src/Allegro.php
@@ -6,6 +6,7 @@ class Allegro
     protected static $clientId;
     protected static $clientSecret;
     protected static $redirectUrl;
+    protected static $curlUrl;
     protected static $authorizationUri = 'https://ssl.allegro.pl/auth/oauth/authorize';
     protected static $tokenUri = 'https://ssl.allegro.pl/auth/oauth/token';
     protected static $header = ['Content-Type: application/x-www-form-urlencoded'];
@@ -16,6 +17,21 @@ class Allegro
         self::$clientSecret=$clientSecret;
         self::$redirectUrl = $redirectUrl;
     }
-
+    protected static function curlAllegro(array $headerArray=[],array $postArray = [])
+    {
+        if(!empty($headerArray))
+            self::$header= $headerArray;
+      //   echo var_dump(self::$header);
+        $curl =  curl_init(self::$curlUrl ?? self::$tokenUri);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, self::$header);
+        if (is_null(self::$curlUrl)) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($postArray));
+        }
+        $result = json_decode(curl_exec($curl));
+        echo var_dump($result);
+        return $result;
+    }
 }
 
--- src/Auth/AllegroAuth.php
+++ src/Auth/AllegroAuth.php
@@ -28,7 +28,7 @@ class AllegroAuth extends Allegro
      */
     public static function token(string $code, bool $refresh = true)
     {
-        $result = self::curlAllegro("POST", [
+        $result = self::curlAllegro([
             'grant_type' => 'authorization_code',
             'code' => $code,
             'redirect_uri' => self::$redirectUrl
@@ -36,25 +36,11 @@ class AllegroAuth extends Allegro
 
         ($refresh) ? self::refreshToken($result->refresh_token) : self::saveData($result);
     }
-
     private static function refreshToken($refresh)
     {
         $data = http_build_query(array('grant_type' => 'refresh_token', 'refresh_token' => $refresh));
         self::$curlUrl = "https://allegro.pl/auth/oauth/token?$data";
         self::saveData(self::curlAllegro());
-    }
-    private static function curlAllegro(string $type = 'GET', array $customArray = [])
-    {
-        $curl =  curl_init(self::$curlUrl ?? self::$tokenUri);
-
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, self::$header);
-        if (is_null(self::$curlUrl)) {
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($customArray));
-        }
-        $result = json_decode(curl_exec($curl));
-        return $result;
     }
     private static function saveData($result)
     {
